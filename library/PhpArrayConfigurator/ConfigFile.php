6a842f959ff6b99beb1b9b492be4f553b774c233
Phil. Austermann <phil@patient-radio.de>
2012-04-12 01:58:19 +0200

    # Updated PHP Doc + Save() - Method

--- library/PhpArrayConfigurator/ConfigFile.php
+++ library/PhpArrayConfigurator/ConfigFile.php
@@ -2,26 +2,37 @@
 
 namespace PhpArrayConfigurator
 {
+	/**
+	 * Active Object Implementation of a php array (config) file
+	 */
 	class ConfigFile
 	{
 		protected $configArray = null;
-		protected $originalFilePath = null;
+		protected $currentFilePath = null;
 
+		/**
+		 * @param string $filePath Specify filename to automatically try to open and read the file on object construction
+		 */
 		public function __construct($filePath = null)
 		{
 			if ($filePath !== null)
 				$this->open ($filePath);
 		}
 
+		/**
+		 * Read PHP Array File (non-blocking file access)
+		 * @param string $filePath
+		 * @return boolean True if array was successfully loaded, False if file could not be read or did not return an array
+		 */
 		public function open($filePath)
 		{
-			$this->originalFilePath = $filePath;
+			$this->currentFilePath = $filePath;
 			if (file_exists($filePath)) {
 				$this->configArray = include $filePath;
 				return true;
 			}
-			else
-				return false;
+
+			return false;
 		}
 
 		/**
@@ -37,21 +48,34 @@ namespace PhpArrayConfigurator
 			return false;
 		}
 
+		/**
+		 * @param array $array
+		 * @return \PhpArrayConfigurator\ConfigFile
+		 */
 		public function setArray(array $array)
 		{
 			$this->configArray = $array;
 			return $this;
 		}
 
-		public function save()
+		/**
+		 * @param type $saveTo If provided, a new copy will be created and then become the current working copy.
+		 * Hint: Developers may want to ensure that no already existing file is overwritten, unless explicitly desired so.
+		 * @return bool False, if the updates could not be written.
+		 */
+		public function save($saveTo = null)
 		{
-			var_dump(CodeCooking\PhpArrayFile::getFromArray($this->configArray));
+			if ($saveTo === null)
+				$saveTo = $this->currentFilePath;
 
-			return $this;
+			$saveSuccess = file_put_contents( $saveTo,
+					CodeCooking\PhpArrayFile::getFromArray($this->configArray)
+			);
+
+			if ($saveSuccess)
+				$this->currentFilePath = $saveTo;
+
+			return $saveSuccess;
 		}
-
-
-
-
 	}
 }
