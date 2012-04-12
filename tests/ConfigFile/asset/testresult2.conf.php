8cce98fa9f4aa7eadd154b3e5eec813c9528417d
Phil <austermann@klimagipfel.de>
2012-04-12 02:28:00 +0200

    # Testrefactoring

---
+++ tests/ConfigFile/asset/testresult2.conf.php
@@ -0,0 +1,15 @@
+<?php
+	return array(
+		'production' => array(
+			'bla' => 'blub',
+			'foo' => 'foovalue',
+			'bar' => 'barvalue',
+			'urls' => array(
+				'one' => 'http://example.com',
+				'two' => 'http://example2.com',
+			),
+			'email' => array(
+				'hghs@idhwi.de', 'dueuihued@ihdiw.com'
+			),
+		),
+	);
