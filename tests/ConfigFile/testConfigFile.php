c4e8e13a60589b2861875521545ba5226c15ec49
Phil. Austermann <phil@patient-radio.de>
2012-04-12 02:28:00 +0200

    # Testrefactoring

--- tests/ConfigFile/testConfigFile.php
+++ tests/ConfigFile/testConfigFile.php
@@ -1,10 +1,11 @@
 <?php
 
-	include '../library/PhpArrayConfigurator/ConfigFile.php';
-	include '../library/PhpArrayConfigurator/CodeCooking/PhpArrayFile.php';
+	include '../../library/PhpArrayConfigurator/ConfigFile.php';
+	include '../../library/PhpArrayConfigurator/CodeCooking/PhpArrayFile.php';
 
-	$configFile = new \PhpArrayConfigurator\ConfigFile('test.conf.php');
+	$configFile = new \PhpArrayConfigurator\ConfigFile('asset/test.conf.php');
+	$configFile->save('asset/testresult1.conf.php');
 
-	var_dump($configFile);
+	$configFile = new \PhpArrayConfigurator\ConfigFile('asset/testresult1.conf.php');
+	$configFile->save('asset/testresult2.conf.php');
 
-	$configFile->save();
